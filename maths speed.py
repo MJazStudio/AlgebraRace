d44845140550b6dfe67de9beeb3fd94b4784a926
MJazStudio <103294407+MJazStudio@users.noreply.github.com>
2023-07-12 07:23:23 +0530

    first github commit
    first github commit

---
+++ maths speed.py
@@ -0,0 +1,44 @@
+import random
+import time
+
+def qst():
+    a = random.randint(2, 12)
+    b = random.randint(2, 12)
+    c = a * b
+    ans = int(input(str(a) + " * " + str(b) + " = "))
+    if ans == c:
+        print("Correct")
+        return True
+    else:
+        print("Wrong")
+        return False
+
+def main():
+    high_score = 0
+    try:
+        with open("high_score.txt", "r") as file:
+            high_score = int(file.read())
+            print("High Score is ", high_score)
+    except FileNotFoundError:
+        pass
+    
+    score = 0
+    start_time = time.time()
+    end_time = start_time + 60
+    
+    while time.time() < end_time:
+        if qst():
+            score += 1
+    
+    print("Time's up!")
+    print("Final Score:", score)
+    
+    if score > high_score:
+        with open("high_score.txt", "w") as file:
+            file.write(str(score))
+            print("New high score achieved!")
+            
+control="y"
+while(control=="y" or control=="Y"):
+    main()
+    control=input("Do you wish to continue(y/n)")
