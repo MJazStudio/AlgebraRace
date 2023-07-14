76de0ba0ea2318160949e5b21faf9d0b0ca0673a
MJazStudio <103294407+MJazStudio@users.noreply.github.com>
2023-07-14 08:02:14 +0530

    Update maths speed.py
    Major changes: Individual high scores are now available. Player can compete with their score and and the best high score

--- maths speed.py
+++ maths speed.py
@@ -14,13 +14,26 @@ def qst():
         return False
 
 def main():
-    high_score = 0
+    player_data = {}
     try:
         with open("high_score.txt", "r") as file:
-            high_score = int(file.read())
-            print("High Score is ", high_score)
+            for line in file:
+                name, best_score = line.strip().split(",")
+                player_data[name] = int(best_score)
+            
+            
+            high_scr_plyr = max(player_data, key=player_data.get)
+            print(f"\nPlayer with the highest score: {high_scr_plyr} ({player_data[high_scr_plyr]})")
     except FileNotFoundError:
         pass
+    
+    player_name = input("Enter your name: ").upper()
+    
+    if player_name not in player_data:
+        player_data[player_name] = 0
+
+    hs_curnt_plyr=player_data[player_name]
+    print("Your best score is ",hs_curnt_plyr)
     
     score = 0
     start_time = time.time()
@@ -33,12 +46,15 @@ def main():
     print("Time's up!")
     print("Final Score:", score)
     
-    if score > high_score:
-        with open("high_score.txt", "w") as file:
-            file.write(str(score))
-            print("New high score achieved!")
-            
-control=input("Are you ready to start(y/n)")
-while(control.lower()=="y"):
+    if score > player_data[player_name]:
+        player_data[player_name] = score
+        print("New personal best score achieved!")
+    
+    with open("high_score.txt", "w") as file:
+        for name, best_score in player_data.items():
+            file.write(f"{name},{best_score}\n")
+
+control = "y"
+while control.lower() == "y":
     main()
-    control=input("Do you wish to continue(y/n)")
+    control = input("Do you wish to continue (y/n): ")
